cc0b5eed3aee929e274c87f002ff11c5c47ecc90
phuongdoan13 <mphuongdoant@gmail.com>
2024-11-03 10:12:22 +1100

    update: removed all code

--- main.go
+++ main.go
@@ -1,25 +1,4 @@
 package main
 
-import (
-	"encoding/base64"
-	"encoding/hex"
-	"fmt"
-)
-
 func main() {
-	input := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d" 
-	output, err := ConvertHexToBase64(input)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(output) 
-}
-
-func ConvertHexToBase64(hexStr string) (string, error){
-	bytes, err := hex.DecodeString(hexStr)
-	if err != nil {
-		return "", err
-	}
-	return base64.StdEncoding.EncodeToString(bytes), nil
 }
