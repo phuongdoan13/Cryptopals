9ab1b1594fef239177fee18ae43cd347aa3046c0
phuongdoan13 <mphuongdoant@gmail.com>
2024-11-03 10:13:10 +1100

    test: add test

---
+++ set1/HexToBase64Converter/HexToBase64Converter_test.go
@@ -0,0 +1,16 @@
+package hextobase64converter
+
+import "testing"
+
+func TestConvertHexStrToBase64Str(t *testing.T) {
+	input := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+	expected := "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+	result, err := ConvertHexStrToBase64Str(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if result != expected {
+		t.Fatalf("Wrong hex to 64 conversion")
+	}
+}
