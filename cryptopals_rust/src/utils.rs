3ac33c889d8a9a2c609d1595f93c6713ee1ddef8
phuongdoan13 <mphuongdoant@gmail.com>
2025-08-14 23:06:39 +1000

    test: add test for s1c1

--- cryptopals_rust/src/utils.rs
+++ cryptopals_rust/src/utils.rs
@@ -5,3 +5,15 @@ pub fn hex_to_base64(hex_str: &str) -> String {
     let bytes = hex::decode(hex_str).expect("Invalid hex");
     general_purpose::STANDARD.encode(bytes)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn set1_challenge1_hex_to_base64() {
+        let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+        let result = hex_to_base64(input);
+        let expect = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+        assert_eq!(result, expect);
+    }
+}
