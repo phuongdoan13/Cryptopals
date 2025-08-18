e1e45ffe110f3dd7a64bd92f615c4c5d219157d1
phuongdoan13 <mphuongdoant@gmail.com>
2025-08-18 22:59:21 +1000

    update: partial done for set1 challeng 3

--- cryptopals_rust/src/main.rs
+++ cryptopals_rust/src/main.rs
@@ -1,5 +1,5 @@
 mod utils;
-
+mod consts;
 fn main() {
     let b64 = utils::hex_to_base64(
         "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d",
--- cryptopals_rust/src/utils.rs
+++ cryptopals_rust/src/utils.rs
@@ -1,6 +1,9 @@
 use std::{io::Bytes, result};
 use base64::{engine::general_purpose, Engine as _};
 use hex;
+use crate::consts::{ASCII_CHARACTER_FREQUENCY};
+use hypors::chi_square::goodness_of_fit;
+
 
 pub fn hex_to_base64(hex_str: &str) -> String {
     let bytes = hex::decode(hex_str).expect("Invalid hex");
@@ -38,6 +41,53 @@ fn xor_bytes_vector_with_different_length(longerBytes: Vec<u8>, shorterBytes: Ve
     return result;
 }
 
+pub fn single_byte_xor_cipher(hexStr: &str) -> (String, u8) {
+    let bytes_result = hex::decode(hexStr);
+    assert!(bytes_result.is_ok(), "input string is not hex.");
+    let input_bytes = bytes_result.unwrap();
+
+    let mut highest_chi_stat: f64 = f64::NEG_INFINITY;;
+    let mut most_probable_byte: u8 = 0 as u8;
+    let mut final_decrypt_string: String = String::new();
+
+    for i in 0..=255 {
+        let candidate_byte = i as u8;
+
+        // xor each byte of the input string with the candidate_byte, and
+        // calculate the frequency of each after_xor_byte;
+        let mut freq_of_input_bytes = vec![0f32; 256];
+        let mut decrypt_string_byte= Vec::new();
+        for b in input_bytes.iter(){
+            let after_xor_byte: u8 = b ^ candidate_byte;
+            decrypt_string_byte.push(after_xor_byte);
+            freq_of_input_bytes[after_xor_byte as usize] += 1f32;
+        }
+
+        // normalise the frequency of each after_xor_byte;
+        for i in 0..=255 {
+            freq_of_input_bytes[i] /= 256f32;
+        }
+
+        // calculate of chi-squared goodness of fit
+        // against the pre-defined distribution of ascii letter
+        let alpha = 0.25; // magical number alpha
+        let chi_gof = goodness_of_fit(freq_of_input_bytes.iter().copied(),
+                                      ASCII_CHARACTER_FREQUENCY.iter().copied(),
+                                      alpha).unwrap();
+
+        if chi_gof.test_statistic > highest_chi_stat {
+            most_probable_byte = candidate_byte;
+            highest_chi_stat =  chi_gof.test_statistic;
+            final_decrypt_string = match str::from_utf8(decrypt_string_byte.as_slice()) {
+                Ok(v) => v.to_string(),
+                Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+            };
+        }
+    }
+
+    (final_decrypt_string, most_probable_byte)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -56,5 +106,14 @@ mod tests {
         let result = fixed_xor(input1, input2);
         let expect = "746865206b696420646f6e277420706c6179";
         assert_eq!(result, expect);
+    }
+
+    #[test]
+    fn set1_challenge3_single_byte_xor_cipher() {
+        let input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+        let (decrypt_message, candidate_byte) = single_byte_xor_cipher(input);
+        let (expected_decrypt_message, expected_byte) = single_byte_xor_cipher
+            (input);
+        assert_eq!(decrypt_message, expected_decrypt_message);
     }
 }
