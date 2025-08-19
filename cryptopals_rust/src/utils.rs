0c46b3307e251110544d48dad770ec46a6d7fe40
phuongdoan13 <mphuongdoant@gmail.com>
2025-08-19 20:29:38 +1000

    update: finish s1c3 single_byte_xor_cipher

--- cryptopals_rust/src/utils.rs
+++ cryptopals_rust/src/utils.rs
@@ -1,20 +1,19 @@
-use std::{io::Bytes, result};
 use base64::{engine::general_purpose, Engine as _};
 use hex;
-use crate::consts::{ASCII_CHARACTER_FREQUENCY};
 use hypors::chi_square::goodness_of_fit;
 
+use crate::consts::{ASCII_CHARACTER_FREQUENCY};
 
 pub fn hex_to_base64(hex_str: &str) -> String {
     let bytes = hex::decode(hex_str).expect("Invalid hex");
     general_purpose::STANDARD.encode(bytes)
 }
 
-pub fn fixed_xor(hexStr1: & str, hexStr2: & str) -> String {
-    let byte1_result = hex::decode(hexStr1);
+pub fn fixed_xor(hex_str1: & str, hex_str2: & str) -> String {
+    let byte1_result = hex::decode(hex_str1);
     assert!(byte1_result.is_ok(), "input string 1 is not hex.");
 
-    let byte2_result = hex::decode(hexStr2);
+    let byte2_result = hex::decode(hex_str2);
     assert!(byte2_result.is_ok(), "input string 2 is not hex");
 
     let byte1 = byte1_result.unwrap();
@@ -24,29 +23,29 @@ pub fn fixed_xor(hexStr1: & str, hexStr2: & str) -> String {
     return hex::encode(resulted_byte);
 }
 
-fn xor_bytes_vector_with_different_length(longerBytes: Vec<u8>, shorterBytes: Vec<u8>) -> Vec<u8> {
-    if longerBytes.len() < shorterBytes.len(){
-        return xor_bytes_vector_with_different_length(shorterBytes, longerBytes);
+fn xor_bytes_vector_with_different_length(longer_bytes: Vec<u8>, shorter_bytes: Vec<u8>) -> Vec<u8> {
+    if longer_bytes.len() < shorter_bytes.len(){
+        return xor_bytes_vector_with_different_length(shorter_bytes, longer_bytes);
     }
 
-    let length_difference = longerBytes.len() - shorterBytes.len();
+    let length_difference = longer_bytes.len() - shorter_bytes.len();
     let mut result : Vec<u8> = Vec::new();
-    for i in 0..longerBytes.len(){
+    for i in 0..longer_bytes.len(){
         if i < length_difference {
-            result.push(longerBytes[i] ^ 0);
+            result.push(longer_bytes[i] ^ 0);
         } else {
-            result.push(longerBytes[i] ^ shorterBytes[i - length_difference]);
+            result.push(longer_bytes[i] ^ shorter_bytes[i - length_difference]);
         }
     }
     return result;
 }
 
-pub fn single_byte_xor_cipher(hexStr: &str) -> (String, u8) {
-    let bytes_result = hex::decode(hexStr);
+pub fn single_byte_xor_cipher(hex_str: &str) -> (String, u8) {
+    let bytes_result = hex::decode(hex_str);
     assert!(bytes_result.is_ok(), "input string is not hex.");
     let input_bytes = bytes_result.unwrap();
 
-    let mut highest_chi_stat: f64 = f64::NEG_INFINITY;;
+    let mut lowest_chi_stat: f64 = f64::INFINITY;
     let mut most_probable_byte: u8 = 0 as u8;
     let mut final_decrypt_string: String = String::new();
 
@@ -75,13 +74,13 @@ pub fn single_byte_xor_cipher(hexStr: &str) -> (String, u8) {
                                       ASCII_CHARACTER_FREQUENCY.iter().copied(),
                                       alpha).unwrap();
 
-        if chi_gof.test_statistic > highest_chi_stat {
-            most_probable_byte = candidate_byte;
-            highest_chi_stat =  chi_gof.test_statistic;
-            final_decrypt_string = match str::from_utf8(decrypt_string_byte.as_slice()) {
-                Ok(v) => v.to_string(),
-                Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-            };
+        match String::from_utf8(decrypt_string_byte) {
+            Ok(s) if chi_gof.test_statistic < lowest_chi_stat => {
+                final_decrypt_string = s;
+                most_probable_byte = candidate_byte;
+                lowest_chi_stat =  chi_gof.test_statistic;
+            }
+            _ => {}
         }
     }
 
@@ -112,8 +111,10 @@ mod tests {
     fn set1_challenge3_single_byte_xor_cipher() {
         let input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
         let (decrypt_message, candidate_byte) = single_byte_xor_cipher(input);
-        let (expected_decrypt_message, expected_byte) = single_byte_xor_cipher
-            (input);
+        let expected_decrypt_message = "Cooking MC's like a pound of bacon";
+        let expected_byte = "X".as_bytes()[0];
+
         assert_eq!(decrypt_message, expected_decrypt_message);
+        assert_eq!(candidate_byte, expected_byte);
     }
 }
